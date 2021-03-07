a2ab7dd77907c835e25968368636402cd8bb06b1
liwenzu <lwzzggml@gmail.com>
2021-03-07 21:51:54 +0300

    update

--- ontology_model/my_work/agents_c_plus/ostis-example-app_0.6.0/problem-solver/cxx/AnswerVerificationModule/agents/QuestionGenerationAgent.cpp
+++ ontology_model/my_work/agents_c_plus/ostis-example-app_0.6.0/problem-solver/cxx/AnswerVerificationModule/agents/QuestionGenerationAgent.cpp
@@ -11,6 +11,7 @@
 #include "utils/Display.hpp"
 #include "QuestionGenerationAgent.hpp"
 #include "keynodes/keynodes.hpp"
+#include "sc-agents-common/utils/LogicRuleUtils.hpp"
 
 using namespace std;
 using namespace utils;
@@ -20,26 +21,51 @@ namespace answerVerificationModule {
     SC_AGENT_IMPLEMENTATION(QuestionGenerationAgent) {
             if (!edgeAddr.IsValid())
                 return SC_RESULT_ERROR;
-
             ScAddr questionNode = ms_context->GetEdgeTarget(edgeAddr);
             ScAddr param = IteratorUtils::getFirstFromSet(ms_context.get(), questionNode);
             if (!param.IsValid())
                 return SC_RESULT_ERROR_INVALID_PARAMS;
-
             ScAddr answer = ms_context->CreateNode(ScType::NodeConstStruct);
             ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, param);
 
 
-            ScIterator5Ptr it_5 = IteratorUtils::getIterator5(ms_context.get(), param, Keynodes::rrel_main_key_sc_element);
-            ScAddr elem, initStruct, resultStruct;
-            if (it_5->Next())
-            {
-                elem = it_5->Get(2);
-                initStruct = ms_context->GetEdgeSource(elem);
-                resultStruct = ms_context->GetEdgeTarget(elem);
-            }
+            ScAddr initStruct = LogicRuleUtils::getIfStatement(ms_context.get(), param);
+            ScAddr resultStruct = LogicRuleUtils::getElseStatement(ms_context.get(), param);
 
-            Display::printEl(ms_context.get(), initStruct);
+
+            ScTemplate initStructTemplate;
+            if (ms_context->HelperBuildTemplate(initStructTemplate, initStruct)) {
+                ScTemplateSearchResult searchResult;
+                if (ms_context->HelperSearchTemplate(initStructTemplate, searchResult)) {
+
+                    cout << "Hello searchResult" << endl;
+
+                    cout << searchResult.Size() << endl;
+
+                    for (int i=0; i<1; i++)
+                    {
+                        ScTemplateSearchResultItem searchResultItem = searchResult[i];
+                        for (int j=0; j<searchResultItem.Size();j++)
+                        {
+                            ScAddr elem = searchResultItem[j];
+
+                            if (!ms_context->HelperCheckEdge(answer, elem, ScType::EdgeAccessConstPosPerm))
+                                ms_context->CreateEdge(ScType::EdgeAccessConstPosPerm, answer, elem);
+
+
+
+
+//                            Display::printNl(ms_context.get(),elem);
+//                            cout << " " << endl;
+                        }
+
+//                        cout << "***************************************" << endl;
+
+                    }
+
+
+                }
+            }
 
             cout << "Hello world" << endl;
 
